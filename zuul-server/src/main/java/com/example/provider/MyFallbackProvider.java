cbe87b741fe45590d2a5874627a59b1ae47e37a8
zhoubin <aa@163.com>
2019-01-03 14:54:40 +0800

    修改内部实现

--- zuul-server/src/main/java/com/example/provider/MyFallbackProvider.java
+++ zuul-server/src/main/java/com/example/provider/MyFallbackProvider.java
@@ -1,5 +1,8 @@
 package com.example.provider;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -7,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,6 +19,10 @@ import java.io.InputStream;
  */
 //@Component
 public class MyFallbackProvider implements FallbackProvider {
+
+    private Logger logger = LoggerFactory.getLogger(MyFallbackProvider.class);
+
+
     @Override
     public String getRoute() {
         //所有的路由都加熔断器,如果只对eureka-client 服务生效，此处可替换为“eureka-client”
@@ -23,18 +31,31 @@ public class MyFallbackProvider implements FallbackProvider {
 
     @Override
     public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
-        return new ClientHttpResponse(){
+        logger.warn(String.format("route:%s,exceptionType:%s,stackTrace:%s", route, cause.getClass().getName(), cause.getStackTrace()));
+        String message = "";
+        if (cause instanceof HystrixTimeoutException) {
+            message = "Timeout";
+        } else {
+            message = "Service exception";
+        }
+        return fallbackResponse(message);
 
+    }
+
+    public ClientHttpResponse  fallbackResponse(final String message) {
+        return new ClientHttpResponse() {
             @Override
             public HttpHeaders getHeaders() {
                 HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.TEXT_PLAIN);
+                headers.setContentType(MediaType.APPLICATION_JSON);
                 return headers;
             }
 
             @Override
             public InputStream getBody() throws IOException {
-                return null;
+
+                String bodyText = String.format("{\"code\": 999,\"message\": \"Service unavailable:%s\"}", message);
+                return new ByteArrayInputStream(bodyText.getBytes());
             }
 
             @Override
@@ -58,5 +79,4 @@ public class MyFallbackProvider implements FallbackProvider {
             }
         };
     }
-
 }
