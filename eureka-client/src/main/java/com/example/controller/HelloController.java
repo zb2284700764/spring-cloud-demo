81fb4d56df010d28abb97575f076ac120a30d3db
zhoubin <aa@163.com>
2019-01-03 14:48:35 +0800

    增加 Thymeleaf 模板

--- eureka-client/src/main/java/com/example/controller/HelloController.java
+++ eureka-client/src/main/java/com/example/controller/HelloController.java
@@ -3,9 +3,11 @@ package com.example.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
@@ -16,9 +18,19 @@ public class HelloController {
 	@Value("${server.port}")
 	String port;
 
-	@RequestMapping("/hi")
-	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+	@RequestMapping("/hi/{name}")
+//	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+	public String home(@PathVariable("name") String name) {
 
 		return "hi " + name + " ,i am from port:" + port;
+	}
+
+
+
+	@RequestMapping("/list")
+	public ModelAndView list(ModelAndView modelAndView) {
+
+		modelAndView.setViewName("modules/demo/demo");
+		return modelAndView;
 	}
 }
